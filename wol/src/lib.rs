e95bb5f02890c8f01d9e77b5a0b636cb93c415da
Haepaxlog <rohdeconstantin@gmail.com>
2024-09-17 19:01:42 +0200

    Sends simple WOL packet and format stuff

--- wol/src/lib.rs
+++ wol/src/lib.rs
@@ -1,5 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core::fmt;
+use std::io::{self, Read};
+use std::net::UdpSocket;
+
+use macaddr::MacAddr6;
+
+pub struct MagicPacket {
+    packet: Vec<u8>,
+}
+
+impl MagicPacket {
+    pub fn from_mac(mac: MacAddr6) -> Self {
+        let preamble = &[0xFF; 6];
+        let magic_seq = mac.into_array().repeat(16);
+
+        let packet = [preamble, magic_seq.as_slice()].concat();
+
+        MagicPacket { packet }
+    }
+
+    pub fn send(&self) -> Result<(), io::Error> {
+        let socket = UdpSocket::bind("0.0.0.0")?;
+        socket.set_broadcast(true)?;
+        socket.send_to(self.packet.as_slice(), "255.255.255.255:9")?;
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for MagicPacket {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let address = self.packet[6..11]
+            .bytes()
+            .map(|x| x.unwrap())
+            .map(|x| x.to_string())
+            .fold(String::from(""), |acc: String, e: String| acc + "-" + &e);
+
+        write!(f, "{}", address)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +44,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test() {
+        todo!()
     }
 }
