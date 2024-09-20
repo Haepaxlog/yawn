93943d7093b4abc013bbb25466a8cfc947238e98
Haepaxlog <rohdeconstantin@gmail.com>
2024-09-20 15:35:28 +0200

    Tests for magic packet

--- wol/src/lib.rs
+++ wol/src/lib.rs
@@ -5,15 +5,17 @@ use std::net::UdpSocket;
 
 use macaddr::MacAddr6;
 
-#[derive(Debug)]
+const MAC_LENGTH: usize = 6;
+const MAGIC_SEQ_MAC_REPETITION: usize = 16;
+
 pub struct MagicPacket {
     packet: Vec<u8>,
 }
 
 impl MagicPacket {
     pub fn from_mac(mac: MacAddr6) -> Self {
-        let preamble = &[0xFF; 6];
-        let magic_seq = mac.into_array().repeat(16);
+        let preamble = &[0xFF; MAC_LENGTH];
+        let magic_seq = mac.into_array().repeat(MAGIC_SEQ_MAC_REPETITION);
 
         let packet = [preamble, magic_seq.as_slice()].concat();
 
@@ -31,7 +33,7 @@ impl MagicPacket {
 
 impl fmt::Display for MagicPacket {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let address_bytes = &self.packet[6..12];
+        let address_bytes = &self.packet[MAC_LENGTH..MAC_LENGTH * 2];
         let mut address = address_bytes
             .bytes()
             .map(|x| x.unwrap())
@@ -46,6 +48,24 @@ impl fmt::Display for MagicPacket {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const PACKET_LENGTH: usize = MAC_LENGTH + MAC_LENGTH * MAGIC_SEQ_MAC_REPETITION;
+
+    #[test]
+    fn magic_packet_check() {
+        let address = MacAddr6::new(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB);
+        let packet = MagicPacket::from_mac(address);
+
+        assert_eq!(packet.packet.len(), PACKET_LENGTH);
+
+        let preamble = &packet.packet[0..MAC_LENGTH];
+        assert_eq!(preamble, [0xFF; 6]);
+
+        let magic_seq = &packet.packet[MAC_LENGTH..];
+        magic_seq
+            .chunks(MAC_LENGTH)
+            .for_each(|x| assert_eq!(x, [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]));
+    }
 
     #[test]
     fn address_format_check() {
