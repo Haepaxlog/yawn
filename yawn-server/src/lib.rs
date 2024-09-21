bd61b6a204b3f4f4d0513ca01d01b4d3bb4ca524
Haepaxlog <rohdeconstantin@gmail.com>
2024-09-21 18:59:09 +0200

    Change to more static server and NATS client

--- yawn-server/src/lib.rs
+++ yawn-server/src/lib.rs
@@ -1,47 +1,31 @@
-use pnet::{
-    datalink::{self, NetworkInterface},
-    ipnetwork::IpNetwork,
-};
-use std::time::Duration;
+use async_nats::{Client, ConnectError, Message, ServerAddr};
+use futures::StreamExt;
 
-struct Server {
-    poll_interval: Duration,
+pub struct Server {
+    outpost_name: String,
+    nats_client: Client
 }
 
 impl Server {
-    pub fn setup(poll_interval: Duration) -> Self {
-        // TODO: Device Network Interfaces ips should be checked asynchronously every poll_interval in case network ifaces
-        // are activated or deactivated while the server is running
-        // TODO: The server should also whenever different ifaces are added or dropped in the new scan
-        // subscribe or unsubsribe to nats subject "yawn.outposts.<ip>/<mask>" of their respective ips
-        Server { poll_interval }
+    pub async fn setup(outpost_name: String, nats_url: ServerAddr) -> Result<Self, ConnectError> {
+        let client = async_nats::connect(nats_url).await?;
+
+        Ok(Server { outpost_name, nats_client: client })
     }
 
-    fn device_network_ifaces(&self) -> Vec<NetworkInterface> {
-        datalink::interfaces()
+    pub async fn run(&self) -> Result<(), async_nats::SubscribeError>{
+
+        let mut subscriber = self.nats_client.subscribe(format!("yawn.outposts.{}", self.outpost_name)).await?;
+
+        while let Some(msg) = subscriber.next().await {
+            println!("{:?}", msg);
+            self.process_req(msg);
+        }
+
+        Ok(())
     }
 
-    fn ip_addresses<F>(&self, filter: F) -> Vec<IpNetwork>
-    where
-        F: Fn(&IpNetwork) -> bool,
-    {
-        self.device_network_ifaces()
-            .clone()
-            .into_iter()
-            .flat_map(|x| {
-                x.ips
-                    .into_iter()
-                    .filter(|ip| !ip.ip().is_loopback() && filter(ip))
-                    .collect::<Vec<_>>()
-            })
-            .collect()
-    }
-
-    fn ipv4_addresses(&self) -> Vec<IpNetwork> {
-        self.ip_addresses(|ip| matches!(ip, IpNetwork::V4(_)))
-    }
-
-    fn ipv6_addresses(&self) -> Vec<IpNetwork> {
-        self.ip_addresses(|ip| matches!(ip, IpNetwork::V6(_)))
+    fn process_req(&self, msg: Message)  {
+        todo!()
     }
 }
--- yawn-server/src/main.rs
+++ yawn-server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, str::FromStr};
+
+use yawn_server::Server;
+
+use async_nats::ServerAddr;
+
+#[tokio::main]
+async fn main() {
+    let nats_url = ServerAddr::from_str(env::var("NATS_URL").expect("NATS_URL wasn't supplied").as_str()).expect("NATS_URL isn't a valid server URL");
+    let outpost_name = env::var("OUTPOST_NAME").expect("OUTPOST_NAME wasn't supplied");
+
+    let server = Server::setup(outpost_name, nats_url).await.expect("Client couldn't connect to nats URL");
+    server.run().await.expect("Server couldn't subscribe to its subject");
 }
